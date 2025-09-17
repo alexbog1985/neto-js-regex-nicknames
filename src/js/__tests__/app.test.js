7126fe320f54d38121d9bd340e3b06653c9ddb33
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2025-09-17 15:39:13 +0300

    completed

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -8,5 +8,22 @@ describe('Validator', () => {
 
   test('правильное имя', () => {
     expect(validator.validateUsername('username')).toBe(true);
+    expect(validator.validateUsername('user-name')).toBe(true);
+    expect(validator.validateUsername('user1name')).toBe(true);
+    expect(validator.validateUsername('user_name')).toBe(true);
+    expect(validator.validateUsername('userName')).toBe(true);
+    expect(validator.validateUsername('Username')).toBe(true);
   });
+
+  test('неправильное имя', () => {
+    expect(validator.validateUsername('1username')).toBe(false);
+    expect(validator.validateUsername('username1')).toBe(false);
+    expect(validator.validateUsername('-username')).toBe(false);
+    expect(validator.validateUsername('username-')).toBe(false);
+    expect(validator.validateUsername('_username')).toBe(false);
+    expect(validator.validateUsername('user%name_')).toBe(false);
+    expect(validator.validateUsername('user&name')).toBe(false);
+    expect(validator.validateUsername('usern4210ame')).toBe(false);
+  });
+
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,15 @@
 export default class Validator {
   validateUsername(username) {
-    // const re = //;
-    return true;
-  };
+    const allowedChars = /^[A-Za-z0-9_-]+$/;
+    const validStart = /^[^0-9-_]/;
+    const validEnd = /[^0-9-_]$/;
+    const overThreeDigits = /^(?!.*\d{4})/;
+
+    return (
+      allowedChars.test(username) &&
+      validStart.test(username) &&
+      validEnd.test(username) &&
+      overThreeDigits.test(username)
+    );
+  }
 }
